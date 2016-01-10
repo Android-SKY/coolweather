037219e20f5dca8b558c83754a4dee21465fccd4
Android-SKY <macan1209@vip.qq.com>
2016-01-11 00:10:47 +0800

    新增数据库帮助类,以及各表对应的实体类.

---
+++ app/src/main/java/com/coolweather/app/model/Province.java
@@ -0,0 +1,34 @@
+package com.coolweather.app.model;
+
+/**
+ * Created by Administrator on 2016/1/10.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
